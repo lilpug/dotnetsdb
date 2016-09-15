56a41a17ac26b1fab45bfc50b5679df2c52976c0
David Whitehead <david__whitehead2@hotmail.com>
2016-09-15 12:35:23 +0100

    added the filetable class into the 2016 version.

---
+++ dotnetsdb/files/lib/SQLServer/2016/FileTable Extension/db/Exists.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DotNetSDB
+{
+    public partial class SqlServer2016 : SqlServerCore
+    {
+        public partial class filetable_extension
+        {
+            public bool database_id_exists(string tableName, string streamID)
+            {
+                try
+                {
+                    db.add_select(tableName, "stream_id");
+                    db.add_where_normal(tableName, "stream_id", streamID);
+                    string id = db.run_return_string();
+                    if (!string.IsNullOrWhiteSpace(id))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw CustomErrorOutput(e);
+                }
+            }
+        }
+    }
+}
